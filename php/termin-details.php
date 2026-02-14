b213b76c2af1ffe82fde0ea8e7d79c5501e6ee69
alaalddienI824 <alaalddien.i824@sabine-blindow-schule.de>
2026-02-14 19:38:38 +0100

    Update termin-details.php

--- php/termin-details.php
+++ php/termin-details.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
-include 'db.php';
+include 'db_config.php';
 
 if (!isset($_SESSION['patient_id'])) {
     header("Location: anmeldung.php");
@@ -181,4 +181,5 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
 </body>
 </html>
+
 
