e062c8275e57344dca27c4ca07cb2278f71f712b
alaalddienI824 <alaalddien.i824@sabine-blindow-schule.de>
2026-03-20 22:23:19 +0100

    Add files via upload

--- JS/script.js
+++ JS/script.js
@@ -1,1 +1,18 @@
+ // JS teil um profil menü zu passen -->
+const profilImg = document.getElementById("profilImg");
+const menu = document.getElementById("profilMenu");
 
+// wenn man den prfilbild klickt
+if (profilImg && menu) {
+  profilImg.addEventListener("click", function (e) {
+    e.stopPropagation();
+    menu.classList.toggle("show");
+  });
+
+// wenn man außerhalb der Menü klickt
+  document.addEventListener("click", function (e) {
+    if (!menu.contains(e.target) && !profilImg.contains(e.target)) {
+      menu.classList.remove("show");   // menü schließen
+    }
+  });
+}
