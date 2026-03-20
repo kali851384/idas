7587f9c8e9d1ff179827f8ed433a381054a2a23a
alaalddienI824 <alaalddien.i824@sabine-blindow-schule.de>
2026-03-20 22:26:15 +0100

    Update Kontakt.php

--- php/Kontakt.php
+++ php/Kontakt.php
@@ -8,36 +8,41 @@ if (!isset($_SESSION['patient_id'])) {
 }                                            
 
 $msg="";
+$type="";
 $vorname=$nachname=$email=$telefon=$betreff=$nachricht="";
 
-// datensätze aus formular lesen
-if ($_SERVER["REQUEST_METHOD"]=="POST") {
-    $vorname=trim($_POST['vorname']?? '');          //trim für leerzeilen am anfang/ende löschen
-    $nachname=trim($_POST['nachname']?? '');
+// datens?tze aus formular lesen
+if($_SERVER["REQUEST_METHOD"]=="POST") {
+    $vorname=trim($_POST['vorname']?? '');          // wenn der benutzer ala schreibt dann wird das ansatt ?? und wenn nix dann '' also lerer String
+    $nachname=trim($_POST['nachname']?? '');       //trim f?r leerzeilen am anfang/ende l?schen
     $email=trim($_POST['email']?? '');
     $telefon=trim($_POST['telefon']?? '');
     $betreff=trim($_POST['betreff']?? '');
     $nachricht=trim($_POST['nachricht']?? '');
 
-// prüfen ob alle felder gefullt
-    if (empty($vorname) || empty($nachname) || empty($email) || empty($betreff) || empty($nachricht)) {
-        $msg= "Bitte alle Pflichtfelder ausfüllen!";    }
+// pr?fen ob alle felder gefullt
+    if(empty($vorname) || empty($nachname) || empty($email) || empty($betreff) || empty($nachricht)) {              // emptay ist wenn lerr oder null oder "" dann $msg
+        $msg= "Bitte alle Pflichtfelder ausf?llen!";   
+        $type="error"; }
 
     
-    // E-Mail prüfen 
-    elseif (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-        $msg = "Ungültige E-Mail-Adresse.";
+    // E-Mail pr?fen 
+    elseif(!filter_var($email, FILTER_VALIDATE_EMAIL)) {   //filter_var ist fertige funktion um email zu ?berpr?fen und FILTER_VALIDATE_EMAIL sichert das es richtig aussieht 
+        $msg = "Ung?ltige E-Mail-Adresse.";
+        $type="error";
     }
-    
-    else {
-        $stmt = $conn->prepare("INSERT INTO kontakt_nachrichten (vorname,nachname,email,telefon,betreff,nachricht)VALUES (?,?,?,?,?,?)");
-        $stmt->bind_param("ssssss", $vorname,$nachname,$email,$telefon,$betreff,$nachricht);
-
+   
+   // wenn alles in formular stimmt dann 
+    else{
+        $stmt = $conn->prepare("INSERT INTO kontakt_nachrichten (vorname,nachname,email,telefon,betreff,nachricht)VALUES (?,?,?,?,?,?)");     // wir bereiten 6-? die leer sind und die werte sp?ter von s bekommen,  die ? sichert den DB falls einer etwas gef?hrlivich im code schreibt (SQL-Injection)      
+        $stmt->bind_param("ssssss", $vorname,$nachname,$email,$telefon,$betreff,$nachricht);     // bind_param bedeutet setze die werte($vorname,,,) im ?
+                                                                    
         if ($stmt->execute()) {
-            $msg = "Nachricht wurde erfolgreich gesendet!";     
-            $vorname=$nachname=$email=$telefon=$betreff=$nachricht="";
-        } else {
-            $msg = "Fehler beim Speichern: " . $stmt->error;
+            $msg = "Nachricht wurde erfolgreich gesendet!"; 
+            $type="success";    
+            $vorname=$nachname=$email=$telefon=$betreff=$nachricht="";    //wenn alles klappt und $msg gezeigt ist dann wird alles wieder leer 
+        } else {                                     
+            $msg = "Fehler beim Speichern: " . $stmt->error;   // wenn fehler dann wird das fehler von mySQl kkopiert und gezeigt
         }
       
     }
@@ -55,7 +60,7 @@ if ($_SERVER["REQUEST_METHOD"]=="POST") {
 </head>
 <body>
 
-<?php include 'header.php'; ?>   <!-- kopf bereich rufen -->
+ <?php include 'header.php'; ?> <!-- kopf bereich rufen -->
 
 <main class="kontakt-container">
 
@@ -64,9 +69,13 @@ if ($_SERVER["REQUEST_METHOD"]=="POST") {
     <p class="kontakt-info">  Für Fragen oder Anliegen stehen wir Ihnen jederzeit gerne zur Verfügung.</p>
 
 
-    <?php if ($msg): ?>
-        <p class="form-nachricht"><?= htmlspecialchars($msg) ?></p>
-    <?php endif; ?>
+            <!-- wenn msg nicht leer ist dann nachrichtzeigen ansonsten nix -->
+            <?php if ($msg): ?>
+                    <div class="form-message <?= $type ?>">
+                        <?= htmlspecialchars($msg) ?>
+                    </div>
+                <?php endif; ?>
+                        
 
     <form id="kontaktForm" method="post" action="kontakt.php">
         <label>Vorname</label><br>
@@ -79,7 +88,7 @@ if ($_SERVER["REQUEST_METHOD"]=="POST") {
         <input type="email" name="email" value="<?= htmlspecialchars($email)?>" required><br>
 
         <label>Telefon</label><br>
-        <input type="tel" name="telefon" value="<?= htmlspecialchars($telefon)?>"><br>
+        <input type="tel" name="telefon" value="<?= htmlspecialchars($telefon)?>" required><br>
 
         <label>Betreff</label><br>
         <input type="text" name="betreff" value="<?=htmlspecialchars($betreff)?>" required><br>
@@ -91,12 +100,25 @@ if ($_SERVER["REQUEST_METHOD"]=="POST") {
     </form>
 </main>
 
-
 <footer id="footer">
    2026 IDAS Gesundheitsportal . Hannover<br>
   Alle Rechte vorbehalten
 </footer>
 
+
+ <script src="script.js"></script>   <!-- JS teil um profil menü zu passen -->
+
+
+<!-- nach den nachricht gesendet wird, wird die nach 5 sekunden verschwenden -->
+ <script>
+setTimeout(() => {
+  const msg = document.querySelector('.form-message');
+  if (msg) {
+    msg.style.transition = "5.0s";
+    msg.style.opacity = "0";
+  }
+}, 3000);
+</script>
 
 </body>
 </html>
