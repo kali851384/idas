464de86e902c023dcb06b6bb7645608f3bbd25ba
alaalddienI824 <alaalddien.i824@sabine-blindow-schule.de>
2026-03-20 15:51:54 +0100

    Update header.php

--- php/header.php
+++ php/header.php
@@ -6,39 +6,44 @@ if (session_status() === PHP_SESSION_NONE) {    // sichern das session funktioni
 
 ?>
 
-<!-- logo -->
-<a href="index.php">
-  <img id="siteLogo" src="LOGO/logo2.png" alt="IDAS Logo" width="72px" height="72px">    <!-- logog -->
-</a>
-  <h1><b>IDAS</b></h1>
-  <h4>Intelligent Doctor Appointment System</h4>
 
-
-<!-- Profil -->
-<div id="navProfil">
-<div id="profilImg">👤</div>
-
-  <div id="profilMenu" class="dropdown">
-    <?php if (isset($_SESSION['patient_id'])): ?>  <!-- wenn eingelogt dann hat er diese 2 wahlen  -->
-      <a href="konto.php">Konto verwalten</a>
-      <a href="logout.php">Abmelden</a>
-    <?php else: ?>                  <!-- wenn nicht dann das -->
-      <a href="anmeldung.php">Anmelden</a>  
-    <?php endif; ?>
-  </div>
-</div>
-
-<!-- Navigation -->
-  <nav id="naviBereich">
-    <div id="navLinks">
-      <a href="index.php">Start</a>
-      <a href="symptome.php">Arzt finden</a>
-      <a href="termine.php">Termine</a>
-      <a href="konto.php">Konto</a>
-      <a href="kontakt.php">Kontakt</a>
+<div id="header">
+    <!-- Logo  -->
+    <div class="header-left">
+        <a href="index.php">
+            <img id="siteLogo" src="LOGO/logo2.png" alt="IDAS Logo" width="72px" height="72px">
+        </a>
+        <h1><b>IDAS</b></h1>
+        <h4>Intelligent Doctor Appointment System</h4>
     </div>
-  </nav>
+    
+    <!--Profil -->
+    <div class="header-right">
+        <div id="navProfil">
+            <div id="profilImg">👤</div>
+            <div id="profilMenu" class="dropdown">
+                <?php if (isset($_SESSION['patient_id'])): ?>
+                    <a href="konto.php">Konto verwalten</a>
+                    <a href="logout.php">Abmelden</a>
+                <?php else: ?>
+                    <a href="anmeldung.php">Anmelden</a>
+                    <a href="registrierung.php">Registrieren</a>
+                <?php endif; ?>
+            </div>
+        </div>
+    </div>
 </div>
+
+<!-- Navigation r -->
+<nav id="naviBereich">
+    <div id="navLinks">
+        <a href="index.php" class="navLink"> Start</a>
+        <a href="symptome.php" class="navLink">Symptome / Arzt finden</a>
+        <a href="termine.php" class="navLink"> Termine</a>
+        <a href="konto.php" class="navLink"> Konto</a>
+        <a href="kontakt.php" class="navLink"> Kontakt</a>
+    </div>
+</nav>
 
 
 
