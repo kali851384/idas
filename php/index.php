f3bc7050e87c446e03c96d496d839e116dc08f5e
alaalddienI824 <alaalddien.i824@sabine-blindow-schule.de>
2026-04-09 21:38:42 +0200

    Update index.php

--- php/index.php
+++ php/index.php
@@ -32,7 +32,7 @@ if ($conn) {
   <meta charset="UTF-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <title>IDAS</title>
-  <link rel="stylesheet" href="style.css" />
+ <link rel="stylesheet" href="../css/style.css" />
 
 
 </head>
