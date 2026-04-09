f379c9596d155ba1e1ea6d020b617ceeaaa5ebd6
alaalddienI824 <alaalddien.i824@sabine-blindow-schule.de>
2026-04-09 21:33:36 +0200

    Update termine.php

--- php/termine.php
+++ php/termine.php
@@ -39,7 +39,7 @@ $result = $stmt->get_result();
   <meta charset="UTF-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <title>Termin�bersicht</title>
-  <link rel="stylesheet" href="style.css" />
+  <link rel="stylesheet" href="../css/style.css" />
   <script src="script.js" defer></script>
 
 
